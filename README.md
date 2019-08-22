f2f7da0c3b9e012ca292a38a232142b22723f7c1
Andreas Wittig <andreas@widdix.de>
2019-08-22 16:34:59 +0200

    preparing release

---
+++ README.md
@@ -0,0 +1,57 @@
+[![Build Status](https://travis-ci.org/cfn-modules/cognito-userpool.svg?branch=master)](https://travis-ci.org/cfn-modules/cognito-userpool)
+[![NPM version](https://img.shields.io/npm/v/@cfn-modules/cognito-userpool.svg)](https://www.npmjs.com/package/@cfn-modules/cognito-userpool)
+
+# cfn-modules: Amazon Cognito User Pool
+
+An Amazon Cognito User Pool with domain and client.
+
+
+## Install
+
+> Install [Node.js and npm](https://nodejs.org/) first!
+
+```
+npm i @cfn-modules/cognito-userpool
+```
+
+## Usage
+
+```
+---
+AWSTemplateFormatVersion: '2010-09-09'
+Description: 'cfn-modules example'
+Resources:
+  Queue:
+    Type: 'AWS::CloudFormation::Stack'
+    Properties:
+      Parameters:
+        InitialUser: 'hello@widdix.de' # optional
+      TemplateURL: './node_modules/@cfn-modules/cognito-userpool/module.yml'
+```
+
+## Parameters
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Default</th>
+      <th>Required?</th>
+      <th>Allowed values</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>InitialUser</td>
+      <td></td>
+      <td>The email address for initial user (temporary password via email).</td>
+      <td>no</td>
+      <td>Valid email address</td>
+    </tr>
+  </tbody>
+</table>
+
+## Limitations
+
+* Secure: Does not backup/snapshot the users stored in the user pool.
---
+++ lambda-src/userpool.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
+const response = require('cfn-response');
+const crypto = require("crypto");
+
+exports.handler = (event, context, cb) => {
+  console.log(`Executing function with event: ${JSON.stringify(event)}`);
+  const error = (err) => {
+    console.log('Error while creating/updating/deleting the user pool domain.', err);
+    response.send(event, context, response.FAILED);
+  };
+  if (event.RequestType === 'Delete') {
+    cognito.deleteUserPoolDomain({
+      Domain: event.PhysicalResourceId,
+      UserPoolId: event.ResourceProperties.UserPoolId
+    }, function(err, data) {
+      if (err) {
+        error(err);
+      } else {
+        console.log(`Deleted custom domain ${domain} for user pool ${event.ResourceProperties.UserPoolId}`);
+        response.send(event, context, response.SUCCESS, {}, event.PhysicalResourceId);
+      }
+    });
+  } else if (event.RequestType === 'Create') {
+    const domain = crypto.randomBytes(5).toString('hex');
+    cognito.createUserPoolDomain({
+      Domain: domain,
+      UserPoolId: event.ResourceProperties.UserPoolId,
+    }, function(err, data) {
+      if (err) {
+        error(err);
+      } else {
+        console.log(`Created custom domain ${domain} for user pool ${event.ResourceProperties.UserPoolId}`);
+        response.send(event, context, response.SUCCESS, {}, domain);
+      }
+    });
+  } else if (event.RequestType === 'Update') {
+    // Updating the domain name is not supported
+    response.send(event, context, response.SUCCESS, {}, event.PhysicalResourceId);
+  } else {
+    error(new Error(`unsupported request type: ${event.RequestType}`));
+  }
+};
